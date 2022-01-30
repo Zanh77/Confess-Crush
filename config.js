ea096a7018ee5a5e882cf606c831aa1a21ca7207
Zanh77 <98698160+Zanh77@users.noreply.github.com>
2022-01-30 22:00:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,11 @@ const CONFIG = {
     desc: 'NHẬP SỐ TIỀN MỌI NGƯỜI MUỐN NHE!! 🥰',
     btnYes: 'Thoát',
     btnNo: 'Tiếp tục',
-    question: 'Oh no, không nhận lì xì sao, thui thì gửi cho An một câu nói trong năm 2022 ik.',
-    btnReply: 'Gửi cho bạn <3',
-    mess: 'Okayyy🥰. Cảm ơn mọi người nhiều nha 😘😘',
-    messDesc: 'Chúc mọi người năm mới thật nhiều hạnh phúc nà',
+    question: 'Oh no, không nhận lì xì sao, thui thì cho cảm nghĩ của mọi người về An ik',
+    reply: 'Mong 
+    btnReply: 'An đẹp troai qué',
+    mess: 'kk mình biết mà',
+    messDesc: 'Okayyy🥰. Cảm ơn mọi người nhiều nha 😘😘',
     btnAccept: 'Ok',
     messLink: 'https://www.facebook.com/profile.php?id=100007421252976' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
