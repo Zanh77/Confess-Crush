06b99e58e9fb748d3d5e76d6fb6799d250a1a07b
Zanh77 <98698160+Zanh77@users.noreply.github.com>
2022-01-30 22:14:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
     titleWeb: "HuỳnhAn",
     introTitle: 'Huỳnh Công An',
-    introDesc: `Happy new year!!!
-    Nhấn tiếp tục để được nhận lì xì của An nè.`,
+    introDesc: `Happy new year!!!`,
     btnIntro: 'Tiếp tục',
     title: 'HAPPY NEW YEAR!!!',
     desc: 'NHẬP SỐ TIỀN MỌI NGƯỜI MUỐN NHE!! 🥰',
     btnYes: 'Thoát',
     btnNo: 'Tiếp tục',
     question: 'Oh no, không nhận lì xì sao, thui thì cho cảm nghĩ của mọi người về An ik',
-    reply: 'An đẹp troai qué',
     btnReply: 'Gửi',
-    mess: 'kk mình biết mà',
-    messDesc: 'Okayyy🥰. Cảm ơn mọi người nhiều nha 😘😘',
+    reply: 'An đẹp troai qué',
+    mess: 'kk mình biết mà.',
+    messDesc: 'Okayyy🥰. Cảm ơn mọi người nhiều nha 😘😘.',
     btnAccept: 'Ok',
     messLink: 'https://www.facebook.com/profile.php?id=100007421252976' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
