2ea2190ab953ccd41bc3631f1402dbd817c0f75e
Zanh77 <98698160+Zanh77@users.noreply.github.com>
2022-01-30 21:50:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const CONFIG = {
     introDesc: `Happy new year!!!
     Nhấn tiếp tục để được nhận lì xì của An nè.`,
     btnIntro: 'Tiếp tục',
-    title: 'NHẬP SỐ TIỀN MỌI NGƯỜI MUỐN NHE!! 🥰',
-    desc: 'Lì xì time',
+    title: 'HAPPY NEW YEAR!!!',
+    desc: 'NHẬP SỐ TIỀN MỌI NGƯỜI MUỐN NHE!! 🥰',
     btnYes: 'Thoát',
     btnNo: 'Tiếp tục',
     question: 'Oh no, không nhận lì xì sao, thui thì gửi cho An một câu nói trong năm 2022 ik.',
     btnReply: 'Gửi cho bạn <3',
-    reply: '...',
+    reply: '',
     mess: 'Okayyy🥰. Cảm ơn mọi người nhiều nha 😘😘',
     messDesc: 'Chúc mọi người năm mới thật nhiều hạnh phúc nà',
     btnAccept: 'Ok',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100007421252976' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
